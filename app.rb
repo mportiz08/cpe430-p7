e53af8b9f10aa41967a782ae67ae476db269c4c5
Marcus Ortiz <mportiz08@gmail.com>
2011-02-26 21:28:25 -0800

    added first model

--- app.rb
+++ app.rb
@@ -3,12 +3,24 @@ require 'sinatra'
 require 'erb'
 require 'data_mapper'
 
-module App
-  get '/' do
-    erb :index
-  end
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Question
+  include DataMapper::Resource
   
-  get '/question/:id' do
-    erb :question
-  end
+  property :id,              Serial
+  property :text,            String
+  property :answer,          String
+  property :total_correct,   Integer
+  property :total_incorrect, Integer
+end
+
+DataMapper.auto_upgrade!
+
+get '/' do
+  erb :index
+end
+
+get '/question/:id' do
+  erb :question
 end
